27acb317f14c2cd3356aea0ed3ea21b822b27493
teaxdev <FloresAlex6904@gmail.com>
2025-09-25 19:27:14 -0500

    finish 02

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,17 @@
 // Write your code here
+
+function Movie(title, director, genre, release, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.release = release;
+    this.rating = rating;
+}
+    
+Movie.prototype.getOverview = function() {
+    return `${this.title} was directed by ${this.director} It is a ${this.genre} film released in ${this.release}. It has a rating of ${this.rating}/10.`;
+}
+
+
+const incep = new Movie("Inception", "Christopher Nolan", "sci-fi", 2010, 8.8);
+console.log(incep.getOverview());
