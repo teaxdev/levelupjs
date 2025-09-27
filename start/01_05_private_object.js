60d169a384574b2393d62524bd8c7aa3e91592b1
teaxdev <FloresAlex6904@gmail.com>
2025-09-27 12:29:44 -0500

    finish 05

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,8 @@
 // Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+
+const user = {[username]: "billybob57", [password]: "pass123", Age: 55};
+
+console.log(user.username)
