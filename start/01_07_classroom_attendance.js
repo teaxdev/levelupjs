fd6f19f626573ae4049ce4a9b17a45537c40fdd2
teaxdev <FloresAlex6904@gmail.com>
2025-09-28 10:14:47 -0500

    finish 07

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -2,5 +2,17 @@
 
 
 function getStudents(classroom) {
-classroom = {}
+let {hasAssistant, classList} = classroom;
+let teacher, assistant, students;
+if (hasAssistant) {
+    [teacher, assistant, ...students] = classList;
+} else {
+    [teacher, ...students] = classList;
 }
+return students;
+}
+
+console.log(getStudents({
+  hasAssistant: false,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"]
+}));
