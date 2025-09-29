cec56f0d39ae22d8bedf2a6a951b92dbbc333c65
teaxdev <FloresAlex6904@gmail.com>
2025-09-29 11:51:16 -0500

    finish 08 and 09

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,10 @@
 // Write your code here
+
+const array = [2, 3, 1, 5]
+
+let coffeeReduce = array.reduce(
+    (accumulator, currentValue) => 
+        accumulator + currentValue
+)
+
+console.log(coffeeReduce * 1.25)
